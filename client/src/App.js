724089357922c9e4fbd8727c3853c8e6c402750d
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2023-05-03 20:13:06 +0300

    Update
    Home, Login, Register pages were created.
    Chakra UI implemented.
    The navbar has been edited.
    Style definitions were made. (reset.css, styles.module.css)

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,26 @@
 import './App.css';
 import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
 import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Login from './pages/Auth/Login';
+import Register from './pages/Auth/Register';
 
 function App() {
   return (
     <Router>
       <div>
         <Navbar />
-
-        <Routes>
-          <Route path="/about" component={Home} />
-        </Routes>
+        <div id='content'>
+          <Routes>
+            <Route path="/" exact element={<Home />} />
+            <Route path="/Login" element={<Login />} />
+            <Route path="/Register" element={<Register />} />
+          </Routes>
+        </div>
       </div>
     </Router>
   );
 }
 
-function Home() {
-  return <h2>Home</h2>;
-}
-
 export default App;
+
--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,15 +1,29 @@
-import React from "react";
 import {Link} from "react-router-dom";
-
+import styles from "./styles.module.css"
+import { Button, ButtonGroup } from '@chakra-ui/react'
 
 function Navbar() {
   return (
-    <nav>
-        <ul>
-            <li>
-                <Link to="/">Home</Link>
-            </li>
+    <nav className={styles.nav}>
+      <div className={styles.left}>
+        <div className="logo">
+          <Link to="/">eCommerce</Link>
+        </div>
+        <ul className={styles.menu}>
+          <li>
+            <Link to="/">Products</Link>
+          </li>
         </ul>
+      </div>
+      <div className={styles.right}>
+        <Link to="/Login">
+          <Button colorScheme='blue'>Login</Button>
+        </Link>
+        <Link to="/Register">
+          <Button colorScheme='blue'>Register</Button>
+        </Link>
+        
+      </div>
     </nav>
   );
 }
