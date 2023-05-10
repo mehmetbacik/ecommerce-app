f85eaea828354a4c7dd34e36b7aa3aabb271edef
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2023-05-10 20:23:40 +0300

    Update
    Product Detail page created.

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Navbar from './components/Navbar';
 import Products from './pages/Products';
+import ProductDetail from './pages/ProductDetail';
 import Login from './pages/Auth/Login';
 import Register from './pages/Auth/Register';
 
@@ -13,6 +14,7 @@ function App() {
         <div id='content'>
           <Routes>
             <Route path="/" exact element={<Products />} />
+            <Route path="/product/:product_id" element={<ProductDetail />} />
             <Route path="/Login" element={<Login />} />
             <Route path="/Register" element={<Register />} />
           </Routes>
--- client/src/api.js
+++ client/src/api.js
@@ -1,6 +1,11 @@
 import axios from "axios";
 export const fetchProductList = async () => {
-	/*const { data } = await axios.get("http://localhost:4000/product");*/
+	/*const { data } = await axios.get(`${proces.env.REACT_APP_BASE_ENDPOINT/product}`);*/
 	const { data } = await axios.get("https://jsonplaceholder.typicode.com/users");
+	return data;
+};
+
+export const fetchProduct = async (id) => {
+	const { data } = await axios.get(`${proces.env.REACT_APP_BASE_ENDPOINT}/product/${id}`);
 	return data;
 };
