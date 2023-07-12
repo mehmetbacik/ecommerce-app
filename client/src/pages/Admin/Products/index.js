256320a722b4df4923c9e6350b3474db60ff4d45
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2023-07-12 23:53:42 +0300

    Update
    Updated Admin:Products page.

--- client/src/pages/Admin/Products/index.js
+++ client/src/pages/Admin/Products/index.js
@@ -1,6 +1,19 @@
 import React from "react";
+import { useQuery } from 'react-query';
+import { fetchProductList } from '../../../api';
+import { DatePicker } from 'antd';
 
 function Products() {
+    const {isLoading, isError, data, error} = useQuery('admin:products', fetchProductList);
+
+    if (isLoading) {
+        return <div>Loading...</div>
+    }
+
+    if (isError) {
+        return <div>Error... {error.message}</div> 
+    }
+
     return (
         <div>
             Products
