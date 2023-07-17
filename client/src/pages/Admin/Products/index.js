2d94d638e537eccf6cc99d9770af82a21f71bfd8
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2023-07-17 22:48:32 +0300

    Update
    Updated admin products page.

--- client/src/pages/Admin/Products/index.js
+++ client/src/pages/Admin/Products/index.js
@@ -1,12 +1,20 @@
 import { useMemo } from "react";
-import { useQuery } from 'react-query';
-import { fetchProductList } from '../../../api';
+import { useQuery, useMutation, useQueryClient } from 'react-query';
+import { fetchProductList, deleteProduct } from '../../../api';
 import { Link } from 'react-router-dom';
 import { Text } from '@chakra-ui/react';
 import { Table, Popconfirm } from 'antd';
 
 function Products() {
+
+    const queryClient = useQueryClient();
+
     const {isLoading, isError, data, error} = useQuery('admin:products', fetchProductList);
+
+    const deleteMutation = useMutation(deleteProduct, {
+        //refetchQueries: ['admin:products'],
+        onSuccess: () => queryClient.invalidateQueries('admin:products'),
+    });
 
     const columns = useMemo(() => {
         return [
@@ -31,7 +39,11 @@ function Products() {
                 render: (text, record) => (
                     <>
                         <Link to={`/admin/products/${record._id}`}>Edit</Link>
-                        <Popconfirm title="Are you sure?" onConfirm={() => {alert("removed");}} onCancel={() => {alert('Cancel!');}} okText="Yes" cancelText="No" placement="left">
+                        <Popconfirm title="Are you sure?" onConfirm={() => {deleteMutation.mutate(record._id, {
+                            onSuccess: () => {
+                                alert('Removed');
+                            }
+                        })}} onCancel={() => {alert('Cancel!');}} okText="Yes" cancelText="No" placement="left">
                             <a href="/#" style={{marginLeft: 10}}>Delete</a>
                         </Popconfirm>
                     </>
