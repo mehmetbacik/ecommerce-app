a74a78e5bced77d9b8660d802aebe5786e1e7513
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2023-07-21 23:24:26 +0300

    Update
    Admin:ProductDetail updated.

--- client/src/pages/Admin/ProductDetail/index.js
+++ client/src/pages/Admin/ProductDetail/index.js
@@ -2,17 +2,57 @@ import React from "react";
 import { useParams } from 'react-router-dom';
 import { fetchProduct } from '../../../api';
 import { useQuery } from 'react-query';
+import { Text, Box, FormControl, FormLabel, Input } from '@chakra-ui/react';
+import { Formik } from 'formik';
 
 function ProductDetail() {
     const {product_id} = useParams();
-    const {isLoading, isError, data, error} = useQuery(['admin:product', product_id]);
+    const {isLoading, isError, data, error} = useQuery(['admin:product', product_id], () => fetchProduct(product_id));
     if (isLoading) {
         return <div>Loading...</div>
     }
     if (isError) {
         return <div>Error {error.message}</div>
     }
-	return <div>ProductDetail</div>;
+    const handleSubmit = () => {
+
+    }
+	return <div>
+        <Text fontSize="2xl">Edit</Text>
+        <Formik 
+            initialValues={{
+                title: data.title,
+                description: data.description,
+                price: data.price,
+                photos: data.photos,
+            }}
+            validationSchema={}
+            onSubmit={handleSubmit}
+        >
+            {
+                ({ handleSubmit, errors, touched, handleChange, handleBlur, values, isSubmitting }) => <>
+                    <Box>
+                        <Box my="5" textAlign="left">
+                            <form onSubmit={handleSubmit}>
+                                <FormControl>
+                                    <FormLabel>
+                                        Title
+                                    </FormLabel>
+                                    <Input 
+                                        name="title"
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                        value={values.title}
+                                        disabled={isSubmitting}
+                                    />
+                                </FormControl>
+                            </form>
+                        </Box>
+                    </Box>
+                </>
+            }
+        </Formik>
+    </div>;
 }
 
 export default ProductDetail;
